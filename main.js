a00f5d3d708cbaba03d6225e1cb3b2efde10ee80
nikolastanin <nikolastanin@gmail.com>
2021-05-10 22:52:03 +0200

    testing

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let currentDate = new Date().getTime();
+let countdownDate = new Date(2022,3,27,17,33,30).getTime();
+let difference = countdownDate-currentDate;
+
+
+
+setInterval( interval = () =>{
+    console.log(countdownDate);
+    let months = ;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+}
+
+,2000);
