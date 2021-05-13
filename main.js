602503cb577c7876e15be8c1cddd633adf199ce0
nikolastanin <nikolastanin@gmail.com>
2021-05-13 10:32:51 +0200

    fixed functions

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
 
 
 let countDate = new Date(2022,3,27,17,33,30).getTime();
-
-let days,hours,minutes,seconds;
-let p = document.getElementById("p");
+let d,h,m,s;
+let dS = document.getElementById("days");
+let hS = document.getElementById("hours");
+let mS = document.getElementById("minutes");
+let sS = document.getElementById("seconds");
 
 const timeCountdown = () =>{
     let currentDate = new Date().getTime();
     let diff = countDate-currentDate;
+        //time calculations
+        d= Math.floor(diff/(1000*60*60*24));
+        h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        s = Math.floor((diff % (1000 * 60)) / 1000);
+      displayingTimes();
         
-        days = Math.floor(diff/(1000*60*60*24));
-        hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-        seconds = Math.floor((diff % (1000 * 60)) / 1000);
-        p.innerHTML = days+"days"+hours+"hours"+minutes+"minutes"+seconds+"secs";
+        
         if(diff<0){
             console.log("timer ended");
         }
+}
+
+const displayingTimes = () =>{
+        dS.innerHTML = d;
+        hS.innerHTML = h;
+        mS.innerHTML = m;
+        sS.innerHTML = s;
 }
 
 setInterval(timeCountdown,1000);
